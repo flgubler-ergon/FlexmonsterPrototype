1ef75157a26b30f1706992b89feb4b60d5dc6501
Florian Gubler <florian.gubler@ergon.ch>
2023-09-28 17:44:15 +0200

    added toolbar tab IDs for demo-purposes

---
+++ src/app/model/ToolbarTabId.ts
@@ -0,0 +1,13 @@
+export type ToolbarTabId =
+    "fm-tab-connect" |
+    "fm-tab-open" |
+    "fm-tab-save" |
+    "fm-tab-export" |
+    "fm-tab-share" |
+    "fm-tab-grid" |
+    "fm-tab-charts" |
+    "fm-tab-format" |
+    "fm-tab-options" |
+    "fm-tab-fields" |
+    "fm-tab-fullscreen"
+
