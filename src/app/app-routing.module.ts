7ad0395d716472f95df1de917e109cc0cd1c9e5b
Florian Gubler <florian.gubler@ergon.ch>
2023-09-28 11:39:16 +0200

    WIP: removed all unnecessary components of the Demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HandlingEventsComponent} from './examples/handling-events/handling-events.component';
-import {CustomizingGridComponent} from './examples/customizing-grid/customizing-grid.component';
-import {CustomizingToolbarComponent} from './examples/customizing-toolbar/customizing-toolbar.component';
-import {PivotTableDemoComponent} from './examples/pivot-table-demo/pivot-table-demo.component';
-import {UpdatingDataComponent} from './examples/updating-data/updating-data.component';
-import {UsingApiCallsComponent} from './examples/using-api-calls/using-api-calls.component';
 import {CustomPivotTableDemoComponent} from './examples/custom-pivot-table-demo/custom-pivot-table-demo.component';
 
 const routes: Routes = [
-  { path: 'pivot-table-demo', component: PivotTableDemoComponent },
   { path: 'demo42', component: CustomPivotTableDemoComponent },
-  { path: 'handling-events', component: HandlingEventsComponent },
-  { path: 'using-api-calls', component: UsingApiCallsComponent },
-  { path: 'updating-data', component: UpdatingDataComponent },
-  { path: 'customizing-toolbar', component: CustomizingToolbarComponent },
-  { path: 'customizing-grid', component: CustomizingGridComponent },
   { path: '**', redirectTo: 'demo42' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FlexmonsterPivotModule } from 'ngx-flexmonster';
-import { SideMenuComponent } from './common/side-menu/side-menu.component';
-import { ToggleButtonComponent } from './common/toggle-button/toggle-button.component';
-import { ToggleSwitchComponent } from './common/toggle-switch/toggle-switch.component';
-import { TopMenuComponent } from './common/top-menu/top-menu.component';
-import { HandlingEventsComponent } from './examples/handling-events/handling-events.component';
-import { CustomizingGridComponent } from './examples/customizing-grid/customizing-grid.component';
-import { CustomizingToolbarComponent } from './examples/customizing-toolbar/customizing-toolbar.component';
-import { PivotTableDemoComponent } from './examples/pivot-table-demo/pivot-table-demo.component';
-import { UpdatingDataComponent } from './examples/updating-data/updating-data.component';
-import { UsingApiCallsComponent } from './examples/using-api-calls/using-api-calls.component';
-import { FormsModule } from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FlexmonsterPivotModule} from 'ngx-flexmonster';
+import {SideMenuComponent} from './common/side-menu/side-menu.component';
+import {TopMenuComponent} from './common/top-menu/top-menu.component';
+import {FormsModule} from '@angular/forms';
 import {CustomPivotTableDemoComponent} from './examples/custom-pivot-table-demo/custom-pivot-table-demo.component';
 import {HttpClientModule} from '@angular/common/http';
 
@@ -23,15 +15,7 @@ import {HttpClientModule} from '@angular/common/http';
     AppComponent,
     TopMenuComponent,
     SideMenuComponent,
-    PivotTableDemoComponent,
     CustomPivotTableDemoComponent,
-    HandlingEventsComponent,
-    UsingApiCallsComponent,
-    UpdatingDataComponent,
-    CustomizingToolbarComponent,
-    CustomizingGridComponent,
-    ToggleButtonComponent,
-    ToggleSwitchComponent
   ],
   imports: [
     BrowserModule,
