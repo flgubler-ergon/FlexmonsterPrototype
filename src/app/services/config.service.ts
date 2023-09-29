24da0968ce545bb8baf86cdf1f9318e1fe78447e
Florian Gubler <florian.gubler@ergon.ch>
2023-09-29 11:14:26 +0200

    added comment

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -11,6 +11,7 @@ export class ConfigService {
 
   constructor(private readonly http: HttpClient) { }
 
+  /** beware: this only works properly if you disable the http cache in the browser... */
   async loadConfig(): Promise<AppConfig> {
     const remoteUrl = `/assets/config.json`
     const response$ = this.http.get<AppConfig>(remoteUrl)
